26ef93f91f1bc722ce35238274961c3462862e30
Jacco de Vries <jdevries@nikhef.nl>
2020-03-05 14:51:01 +0100

    updated plots

--- analysis/plotEcal.py
+++ analysis/plotEcal.py
@@ -13,6 +13,9 @@ tree = f.Get("Bd_Kstee/DecayTree")
 
 bins = 50
 
+zoom = 0 # zoom around electron track @ ECAL. set to 0 for full ECAL view
+
+
 ###################################
 
 cECAL = ROOT.TCanvas("cECAL")
@@ -25,22 +28,37 @@ for entry in tree :
   photonxlist = entry.ECAL_photon_x_arr
   photonylist = entry.ECAL_photon_y_arr
   photonptlist = entry.ECAL_photon_PT_arr
-  
+  photoncllist = entry.ECAL_photon_CL_arr
+
+  xwindow = [-4000, 4000]
+  ywindow = [-3200, 3200]
+  if(zoom) :
+    meanx = entry.eminus_ECAL_x
+    meany = entry.eminus_ECAL_y
+    xwindow = [meanx - zoom, meanx + zoom]
+    ywindow = [meany - zoom, meany + zoom]
+ 
+  # fill clusters
   #hECAL = ROOT.TH2F("hECAL","ECAL clusters", bins,-4000,4000, bins,-3200,3200)
   #for i in range(entry.N_ECAL_clusters) :
   #  hECAL.Fill( clusterxlist[i], clusterylist[i], clusterelist[i] )
-  hECAL = ROOT.TH2F("hECAL","ECAL photons", bins, -4000,4000, bins,-3200,3200)
+
+  # fill photons
+  hECAL = ROOT.TH2F("hECAL","ECAL photons", bins, xwindow[0], xwindow[1], bins, ywindow[0], ywindow[1])
   for i in range(entry.N_ECAL_photons) :
     #if(entry.ECAL_photon_CL_arr[i] > 0.1 and entry.ECAL_photon_PT_arr[i] > 75.) : # BremAdder def. cuts
       hECAL.Fill( photonxlist[i], photonylist[i], photonptlist[i] )
+      #hECAL.Fill( photonxlist[i], photonylist[i], photoncllist[i] )
 
   hECAL.GetXaxis().SetTitle("x [mm]")
   hECAL.GetYaxis().SetTitle("y [mm]")
   #hECAL.GetZaxis().SetTitle("E")
   hECAL.GetZaxis().SetTitle("PT")
+  #hECAL.GetZaxis().SetTitle("CL")
   hECAL.SetMinimum(0.)
   #hECAL.SetMaximum(20000.)
   hECAL.SetMaximum(1000.)
+  #hECAL.SetMaximum(1.)
   hECAL.Draw("COLZ")
   cECAL.SetFixedAspectRatio()
 
@@ -64,12 +82,13 @@ for entry in tree :
   electron_TT_x += 2*xslope*entry.eminus_ECAL_TTtrack_sprx
   electron_TT_y += 2*yslope*entry.eminus_ECAL_TTtrack_spry
   electronvelograph = ROOT.TGraph(1, array('f',[electron_velo_x]), array('f',[electron_velo_y]))
-  electronvelograph.SetMarkerStyle(32)
+  electronvelograph.SetMarkerStyle(27)
+  electronvelograph.SetMarkerColor(8)
   electronvelograph.SetMarkerSize(1)
   electronvelograph.Draw("Psame")
-  
   electronTTgraph = ROOT.TGraph(1, array('f',[electron_TT_x]), array('f',[electron_TT_y]))
-  electronTTgraph.SetMarkerStyle(26)
+  electronTTgraph.SetMarkerStyle(27)
+  electronTTgraph.SetMarkerColor(9)
   electronTTgraph.SetMarkerSize(1)
   electronTTgraph.Draw("Psame")
  
@@ -97,7 +116,7 @@ for entry in tree :
   if(entry.eminus_MCphotondaughters_N > 0) : 
     print(" * Has MC photon daughters: {0}".format(entry.eminus_MCphotondaughters_N))
     for i in range(entry.eminus_MCphotondaughters_N) :
-      print("  - PT = {0:.0f}, origin = ({1:.2f},{2:.2f},{3:.2f}), atECAL = ({4:.2f},{5:.2f})".format(
+      print("  - PT = {0:.0f},  origin = ({1:.2f},{2:.2f},{3:.2f}), atECAL = ({4:.2f},{5:.2f})".format(
         entry.eminus_MCphotondaughters_PT[i], 
         entry.eminus_MCphotondaughters_orivx_X[i], entry.eminus_MCphotondaughters_orivx_Y[i], entry.eminus_MCphotondaughters_orivx_Z[i], 
         entry.eminus_MCphotondaughters_ECAL_X[i], entry.eminus_MCphotondaughters_ECAL_Y[i] ))
